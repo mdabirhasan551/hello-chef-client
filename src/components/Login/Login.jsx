af639624de76180c0710de683bd7110ad70811c2
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-18 14:01:52 +0600

    login and register page setted up

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Form, Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-           <h1>This is login</h1> 
-        </div>
-    );
+  return (
+    <div className="d-flex p-5 m-5 justify-content-center">
+      <div>
+        <p className="text-center fs-1">
+          <b>Please Login</b>
+        </p>
+        <Form>
+          <input
+            type="email"
+            name="email"
+            className="form-control m-2"
+            placeholder="Enter Your Email"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            className="form-control m-2"
+            placeholder="Enter Password"
+            required
+          />
+          <Button className="m-2 btn-warning" type="submit">
+            Login
+          </Button>
+          <p>
+            Don't Have an account? <Link to="/register">Register Here</Link>
+          </p>
+          <p className='text-center'><Button className='btn-success p-2'>Login with google <FaGoogle className='m-2'/></Button></p>
+          <p className='text-center'><Button className='btn-dark p-2'>Login with Github<FaGithub className='m-2'/></Button></p>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
+import { Form, Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h1>Register coming soon</h1>
+            <div className="d-flex p-5 m-5 justify-content-center">
+      <div>
+        <p className="text-center fs-1">
+          <b>Please Register</b>
+        </p>
+        <Form >
+          <input
+            type="text"
+            name="name"
+            className="form-control m-2"
+            placeholder="Enter Your name"
+          />
+          <input
+            type="email"
+            name="email"
+            className="form-control m-2"
+            placeholder="Enter Your Email"
+          />
+          <input
+            type="password"
+            name="password"
+            className="form-control m-2"
+            placeholder="Enter Password"
+          />
+          <Button className="m-2 btn-warning" type="submit">
+            Register 
+          </Button>
+          <p>
+            Already Have an account? <Link to="/login">Login Here</Link>
+          </p>
+          <p className='text-center'><Button className='btn-success p-2'>Register with google <FaGoogle className='m-2'/></Button></p>
+          <p className='text-center'><Button className='btn-dark p-2'>Register with Github<FaGithub className='m-2'/></Button></p>
+        </Form>
+      </div>
+    </div>
         </div>
     );
 };
